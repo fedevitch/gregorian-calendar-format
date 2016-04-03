99a7ae58cc80b777c5e219abd5539cf9f6f53c4d
Lyubomyr Fedevitch <lmfedevitch@gmail.com>
2016-04-03 16:55:06 +0300

    ukrainian locale added

---
+++ src/locale/uk_UA.js
@@ -0,0 +1,20 @@
+/*
+ * ua-UA locale
+ * @ignore
+ * @author fedevitch@ukr.net
+ */
+module.exports = {
+  eras: ['до н.е.', 'н.е.'],
+  months: ['Січеньy', 'Лютий', 'Березень', 'Квітень', 'Травень', 'Червень', 'Липень',
+    'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудень'],
+  shortMonths: ['Січ', 'Лют', 'Бер', 'Кві', 'Тра', 'Чер', 'Лип', 'Сер', 'Вер',
+    'Жов', 'Лис', 'Гру'],
+  weekdays: ['Неділя', 'Понеділок', 'Вівторок', 'Середа', 'Четвер', 'П\'ятниця',
+    'Субота'],
+  shortWeekdays: ['Нед', 'Пон', 'Вів', 'Сер', 'Чет', 'Птн', 'Суб'],
+  veryShortWeekdays: ['Нд', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб'],
+  ampms: ['AM', 'PM'],
+  datePatterns: ['EEEE, d MMMM, yyyy', 'd MMMM, yyyy', 'd MMM, yyyy', 'd-M-yy'],
+  timePatterns: ['h:mm:ss a \'GMT\'Z', 'h:mm:ss a', 'h:mm:ss a', 'h:mm a'],
+  dateTimePattern: '{date} {time}',
+};
